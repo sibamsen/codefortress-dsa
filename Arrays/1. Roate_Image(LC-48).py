a6464f0cc839d2581f5490f5b158927af105bfe0
Sibam Sen <90177493+sibamsen@users.noreply.github.com>
2026-05-01 23:31:56 +0530

    Implement image rotation in place

---
+++ Arrays/1. Roate_Image(LC-48).py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n=len(matrix)
+        #  Step 1: Transpose
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+        # Step 2: Reverse each row manually
+        for i in range(n):
+            left=0
+            right=n-1
+
+            while left<right:
+                matrix[i][left],matrix[i][right]=matrix[i][right],matrix[i][left]
+                left+=1
+                right-=1
