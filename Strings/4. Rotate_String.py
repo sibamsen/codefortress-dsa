00f491d2c3f1e51a5728f4eb8abde7177082b474
Sibam Sen <90177493+sibamsen@users.noreply.github.com>
2026-06-02 23:40:23 +0530

    Convert rotateString to a method in Solution class
    Refactor rotateString function into a class method.

--- Strings/4. Rotate_String.py
+++ Strings/4. Rotate_String.py
@@ -14,16 +14,11 @@ If goal is substring of s+s → rotation possible
 Time Complexity: O(n)
 Space Complexity: O(n)
 '''
-
-def rotateString(s, goal):
-    if len(s) != len(goal):
-        return False
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
     
-    return goal in (s + s)
+        return goal in (s + s)
 
 
-# Driver Code
-if __name__ == "__main__":
-    s = "abcde"
-    goal = "cdeab"
-    print(rotateString(s, goal))
